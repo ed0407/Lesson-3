7ebab0267be82f7f9487932248ac65b15e063118
Your Name <you@example.com>
2018-12-08 18:27:24 +0400

    added fstests.js

---
+++ fstests.js
@@ -0,0 +1,9 @@
+var obj = 
+{
+    "first name":"Vardan",
+    "last name":"Hovsepyan",
+    "age":13,
+    "tumo_student":true
+}
+
+module.exports = JSON.stringify(obj);
--- index.js
+++ index.js
@@ -28,7 +28,7 @@
 //      res.redirect('http://google.com');
 //  })
 //  app.get("/*", function(req,res){
-//     res.send("<h1>Error 404</h1");
+//     res.status(404).send("<h1>Error 404</h1>");
 // })
 
 // app.listen(3000,function(){
@@ -36,15 +36,72 @@
 // });
 
 //Project_________________________________
-var express = require("express");
-var app = express();
+// var express = require("express");
+// var app = express();
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.get("/", function(req, res){
-   res.redirect("index.html");
-});
+// app.get("/", function(req, res){
+//    res.redirect("index.html");
+// });
 
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
-});
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+
+//_______________________________
+
+// var Square = require("./module");
+// var mySquareObject = new Square(5);
+
+// function main() {
+//    console.log(mySquareObject.getArea());
+// }
+// main();
+
+//____________________________
+
+// var fs = require('fs');
+
+// function main(){
+//    fs.writeFile("hello.txt", "Hello world\n", function(err){
+//        console.log("fs.writeFile ended");
+//    });
+//    console.log("fs.writeFile");
+// }
+// main();
+
+//__________________________
+
+// var fs = require('fs');
+
+// function main() {
+//    fs.writeFileSync("hello.txt", "Hello world\n");
+//    console.log("fs.writeFile");
+// }
+// main();
+
+//____________________________
+
+// var fs = require('fs');
+// var dummyText = "Apple yep";
+
+// function main(){
+//     fs.writeFileSync("dummytext.txt",dummyText);
+//     var text = fs.readFileSync("dummytext.txt").toString();
+//     console.log(dummyText == text);
+//     console.log(text);
+//     fs.writeFileSync("undummytext.txt",
+//         text.replace("Apple","Microsoft")
+//     );
+// }
+// main();
+
+var f = require('./fstests');
+var fs = require('fs');
+
+function main(){
+    fs.writeFileSync("obj.json",f)
+    console.log("+++");
+}
+main();
